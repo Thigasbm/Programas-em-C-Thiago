e6a598a1862c024811dadb6fd355372a6ad42603
Thiago Maran <129339926+Thigasbm@users.noreply.github.com>
2025-06-02 20:27:42 -0300

    exercicio6ESTRELA.c

--- SuperExercicioEstrelaTHIAGOMARAN.c
+++ SuperExercicioEstrelaTHIAGOMARAN.c
@@ -29,32 +29,25 @@ int main(){
         i++;
     }
 
-    /** 
-    //apresenta quantos carneiros cada estrela tem
-    for(i=0;i<qtd_estrelas;i++){
-        printf("%d ",estrela[i]);
-    }
-    */
-
     printf("\n---------------------Resultado---------------------\n");
 
     i=0;
     while(i>=0 && i<qtd_estrelas){  //enquanto i for maior que 0 (em algum momento, ele vai zerar), i for menor que qtd_estrelas     
         if(estrela[i]>0){           //se for maior que 0
-            if(atacadas[i]==0){     //se atacadas[i] for 0 (não foi atacado ainda) - todos os [i] são 0. então viram 1 para definir "atacado"
-                atacadas[i]=1;      //define atacadas[i] como 1 (atacado)
+            if(atacadas[i]==0){     //se atacadas[i] for 0 (não foi atacado ainda) - todos os [i] são 0. então viram 1 para definir "atacado" | é basicamente boolean
+                atacadas[i]=1;      //define atacadas[i] como 1 (atacado) | define o bool para true
                 total_atk++;        //incrementa total_atk
             }
 
-            ultima = estrela[i]; //salva o valor da estrela[i] em ultima
-            estrela[i]--;        //desincrementa estrela[i] (rouba)
+            ultima = estrela[i];    //salva o valor da estrela[i] em ultima
+            estrela[i]--;           //desincrementa estrela[i] (rouba)
 
-            if(ultima%2==0){     //se o resto for 0:
+            if(ultima%2==0){        //se o resto for 0:
                 i--;
             }else{
                 i++;
             }
-        }else{                   //se estrela[i] for 0, vai repetir o processo, mas sem contabilizar a var total_atk
+        }else{                      //se estrela[i] for 0, vai repetir o processo, mas sem contabilizar a var total_atk
             if (estrela[i]%2==0){
                 i--;
             }else{
